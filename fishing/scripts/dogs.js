b42220e7d6574513e2790c570195792f289092b8
Mallory Miller <mallorysilvermiller@gmail.com>
2024-09-20 18:01:34 -0400

    maybe solvable?

--- fishing/scripts/dogs.js
+++ fishing/scripts/dogs.js
@@ -8,7 +8,7 @@ function first_speak() {
         {"speaker": "R3TRVR", "text": "My crew and I were trying to leave orbit when our ship was suddenly attacked by some kind of virus."} ,
         {"speaker": "R3TRVR", "text": "It wanted us to solve a puzzle, so I split the work amongst the crew."} , 
         {"speaker": "R3TRVR", "text": "Unfortunately... we ran out of time before we could reconvene with our progress."} , 
-        {"speaker": "R3TRVR", "text": "All I know is that <span style='color: red'> the first letter is either 10 letters before the second one in the alphabet or it is an R.</span>"} , 
+        {"speaker": "R3TRVR", "text": "All I know is that <span style='color: red'> the first letter is either 10 letters before the second one or it is an R.</span>"} , 
         {"speaker": "R3TRVR", "text": "Got that?"} , 
 
         [{choice: "Got what?", onclick: "changeText(-2)"},{choice: "Got it.", onclick: "changeText(1)"}],
@@ -18,24 +18,25 @@ function first_speak() {
 
 function second_speak() {
     newText([
-        {"speaker": "Cnvs", "text": "That was so much fun! Can we go again?"} ,  
+        {"speaker": "0ZI3", "text": "That was so much fun! Can we go again?"} ,  
         [{choice: "Any progress on the ship's puzzle?", onclick: "changeText(3)"},{choice: "Again?!", onclick: "changeText(1)"}],
-        {"speaker": "Cnvs", "text": "Again! Again!"} , 
+        {"speaker": "0ZI3", "text": "Again! Again!"} , 
         [{choice: "Any progress on the ship's puzzle?", onclick: "changeText(1)"},{choice: "Again?!?", onclick: "changeText(-1)"}],
-        {"speaker": "Cnvs", "text": "Yeah I know it! You want it?"} , 
-        {"speaker": "Cnvs", "text": "Aww but where's the fun if you don't solve it yourself! Hmm..."} , 
-        {"speaker": "Cnvs", "text": "Oh! Oh! Here: <span style='color: red'>The fourth letter can be found by looking at the second half of the first!</span>"} , 
-        {"speaker": "Cnvs", "text": "Hope that helps, friend! Again, Again!"} , 
+        {"speaker": "0ZI3", "text": "Yeah I know it! You want it?"} , 
+        {"speaker": "0ZI3", "text": "Aww but where's the fun if you don't solve it yourself! Hmm..."} , 
+        {"speaker": "0ZI3", "text": "Oh! Oh! Here: <span style='color: red'>The fourth letter can be found by looking at the second half of the first!</span>"} , 
+        {"speaker": "0ZI3", "text": "Hope that helps, friend! Again, Again!"} , 
     ])
 }
+
 function third_speak() {
     newText([
-        {"speaker": "Cnvs", "text": "Ah what a valiant rescue! A bravest of endeavors!"} , 
-        {"speaker": "Cnvs", "text": "What a story this shall make, generations shall sing your praise, kind stranger!"} , 
+        {"speaker": "T3RI3R", "text": "Ah what a valiant rescue! A bravest of endeavors!"} , 
+        {"speaker": "T3RI3R", "text": "What a story this shall make, generations shall sing your praise, kind stranger!"} , 
         [{choice: "Did you make any progress on the puzzle in the ship?", onclick: "changeText(2)"},{choice: "Oh, do say more.", onclick: "changeText(1)"}],
-        {"speaker": "Cnvs", "text": "A splendiferous occasion! A phenomenal and courageous venture!"} , 
-        {"speaker": "Cnvs", "text": "Of course for your incredible endeavors I must bequeath you a reward:"} , 
-        {"speaker": "Cnvs", "text": "<span style='color: red'>The last letter is the first plus seventeen!</span>"} , 
+        {"speaker": "T3RI3R", "text": "A splendiferous occasion! A phenomenal and courageous venture!"} , 
+        {"speaker": "T3RI3R", "text": "Of course for your incredible endeavors I must bequeath you a reward:"} , 
+        {"speaker": "T3RI3R", "text": "<span style='color: red'>The last letter is the first plus seventeen!</span>"} , 
     ])
 }
 
@@ -43,7 +44,7 @@ function fourth_speak() {
     newText([
         {"speaker": "M4L4MUt3", "text": "Hi Hi!"} , 
         {"speaker": "M4L4MUt3", "text": "..."} , 
-        [{choice: "Did you have any information about the letter you were decoding?", onclick: "changeText(1)"},{choice: "Bye.", onclick: "changeText(-10)"}],
+        [{choice: "Did you have any information about the letter you were decoding?", onclick: "changeText(1)"},{choice: "...", onclick: "changeText(-1)"}],
         {"speaker": "M4L4MUt3", "text": "Oh. Uhh. Haha."} , 
         {"speaker": "M4L4MUt3", "text": "Funny story I, uhm, usually just ask the onboard ship assistant to do that kind of thing."} , 
         {"speaker": "M4L4MUt3", "text": "But for some reason it was offline, so."} , 
@@ -60,6 +61,15 @@ function fifth_speak() {
     ])
 }
 
+function sixth_speak() {
+    newText([
+        {"speaker": "D4CH", "text": "Greetings, friend. Thank you for the assist."} , 
+        {"speaker": "D4CH", "text": "I presume you're collecting what the crew found of the puzzle on board?"} , 
+        {"speaker": "D4CH", "text": "I was very close, but my diction is rather poor. <span style='color: red'>The third letter fits in the blank: ac_te</span>."} , 
+        {"speaker": "D4CH", "text": "Hopefully that helps; thank you for your service."} , 
+    ])
+}
+
 function no_more() {
     newText([
         {"text": "You've fished... a boot?"} , 
@@ -68,7 +78,7 @@ function no_more() {
 }
 
 
-const speakers = [first_speak, second_speak, third_speak, fourth_speak, fifth_speak, no_more]
+const speakers = [first_speak, second_speak, third_speak, fourth_speak, fifth_speak, sixth_speak, no_more];
 var current_speaker = 0;
 
 function dog_speak() {
